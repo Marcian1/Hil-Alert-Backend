f126f5824e4afea2d8f269c5d2edc0ee2af5649d
Marcian Bondoc <marcian.bondoc@gmail.com>
2021-11-08 03:09:32 +0200

    Small fixes in coordination with front end register and forgot password features

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,22 +4,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
-
 use Illuminate\Support\Facades\Hash;
 use App\User;
+use App\Http\Resources\UserResource;
 use App\Http\Requests\RegisterRequest;
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
         
-        if (Auth::attempt($request->only('email', 'password'))) {
-            $user = Auth::user();
-          
+        if (Auth::attempt($request->only('email', 'password','username'))) {
+            $user = Auth::user();        
             $token = $user->createToken('admin')->accessToken;
-
+            
             return [
                 'token' => $token,
+                new UserResource($user)
             ];
         }
 
@@ -30,11 +30,12 @@ class AuthController extends Controller
 
     public function register(RegisterRequest $request)
     {
+
         $user = new User;
         $user->username=$request->username;
         $user->password=Hash::make($request->password);
         $user->email=$request->email;
-  
+      
 
         $user->save();
   
